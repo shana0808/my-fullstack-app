6c3622bc6adaec7a52f38c529cc5b8ba461bc73b
Shahanaz Sheriff <shahanaz0908@gmail.com>
2025-06-28 14:39:25 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
-    result = ""
-    if request.method == "POST":
-        text = request.form["text"]
-        if "good" in text.lower():
-            result = "Positive 😊"
-        elif "bad" in text.lower():
-            result = "Negative 😢"
-        else:
-            result = "Neutral 😐"
-    return render_template("index.html", result=result)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
